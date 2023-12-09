a81f855721aa22fffe2673f6c868a68e3b760fec
Sterbeed <mohamedaem2004@gmail.com>
2023-12-09 13:28:25 -0500

    better animation, longer model

--- src/main/java/net/sterbee/catpocalypse/entity/client/PykeModel.java
+++ src/main/java/net/sterbee/catpocalypse/entity/client/PykeModel.java
@@ -21,7 +21,7 @@ public class PykeModel<T extends PykeEntity> extends SinglePartEntityModel<T> {
         ModelPartData modelPartData = modelData.getRoot();
         ModelPartData pyke = modelPartData.addChild("pyke", ModelPartBuilder.create(), ModelTransform.pivot(1.0F, 24.0F, 0.0F));
 
-        ModelPartData head = pyke.addChild("head", ModelPartBuilder.create().uv(0, 15).cuboid(-3.0F, -14.0F, -1.0F, 4.0F, 4.0F, 4.0F, new Dilation(0.0F)), ModelTransform.of(0.0F, 5.0F, -7.8F, 0.0436F, 0.0F, 0.0F));
+        ModelPartData head = pyke.addChild("head", ModelPartBuilder.create().uv(0, 15).cuboid(-3.0F, -14.0F, -1.0F, 4.0F, 4.0F, 4.0F, new Dilation(0.0F)), ModelTransform.of(0.0F, 5.0F, -9.8F, 0.0436F, 0.0F, 0.0F));
 
         ModelPartData nose = head.addChild("nose", ModelPartBuilder.create().uv(3, 4).cuboid(-1.5F, -12.25F, -1.25F, 1.0F, 1.0F, 1.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, 0.0F, 0.0F));
 
@@ -44,24 +44,24 @@ public class PykeModel<T extends PykeEntity> extends SinglePartEntityModel<T> {
         ModelPartData rightwhiskers = whiskers.addChild("rightwhiskers", ModelPartBuilder.create().uv(29, 18).cuboid(-4.0F, -7.0F, -9.0F, 1.0F, 2.0F, 0.0F, new Dilation(0.0F))
                 .uv(29, 18).cuboid(-3.5F, -6.5F, -8.9F, 1.0F, 1.0F, 0.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, 0.0F, 0.0F));
 
-        ModelPartData body = pyke.addChild("body", ModelPartBuilder.create().uv(0, 0).cuboid(-2.75F, -7.25F, -6.0F, 3.5F, 4.0F, 11.0F, new Dilation(0.0F))
-                .uv(19, 0).cuboid(-2.9F, -7.5F, 4.0F, 3.8F, 4.5F, 2.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, 0.0F, 0.0F));
+        ModelPartData body = pyke.addChild("body", ModelPartBuilder.create().uv(-4, -4).cuboid(-2.75F, -7.25F, -8.0F, 3.5F, 4.0F, 15.0F, new Dilation(0.0F))
+                .uv(19, 0).cuboid(-2.9F, -7.5F, 6.0F, 3.8F, 4.5F, 2.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, 0.0F, 0.0F));
 
         ModelPartData legs = pyke.addChild("legs", ModelPartBuilder.create(), ModelTransform.pivot(0.0F, 0.0F, 0.0F));
 
-        ModelPartData topright = legs.addChild("topright", ModelPartBuilder.create().uv(20, 21).cuboid(-3.0F, -6.5F, -5.0F, 1.0F, 6.5F, 2.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, 0.0F, 0.0F));
+        ModelPartData topright = legs.addChild("topright", ModelPartBuilder.create().uv(20, 21).cuboid(-3.0F, -6.5F, -7.0F, 1.0F, 6.5F, 2.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, 0.0F, 0.0F));
 
-        ModelPartData topleft = legs.addChild("topleft", ModelPartBuilder.create().uv(14, 21).cuboid(0.0F, -6.75F, -5.0F, 1.0F, 6.75F, 2.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, 0.0F, 0.0F));
+        ModelPartData topleft = legs.addChild("topleft", ModelPartBuilder.create().uv(14, 21).cuboid(0.0F, -6.75F, -7.0F, 1.0F, 6.75F, 2.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, 0.0F, 0.0F));
 
-        ModelPartData bottomleft = legs.addChild("bottomleft", ModelPartBuilder.create().uv(0, 5).cuboid(-0.25F, -4.0F, 4.5F, 1.0F, 4.0F, 1.5F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, 0.0F, 0.0F));
+        ModelPartData bottomleft = legs.addChild("bottomleft", ModelPartBuilder.create().uv(7, 25).cuboid(-0.25F, -4.0F, 6.5F, 1.0F, 4.0F, 1.5F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, 0.0F, 0.0F));
 
-        ModelPartData bottomright = bottomleft.addChild("bottomright", ModelPartBuilder.create().uv(0, 23).cuboid(-2.75F, -4.0F, 4.5F, 1.0F, 4.0F, 1.5F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, 0.0F, 0.0F));
+        ModelPartData bottomright = bottomleft.addChild("bottomright", ModelPartBuilder.create().uv(0, 23).cuboid(-2.75F, -4.0F, 6.5F, 1.0F, 4.0F, 1.5F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, 0.0F, 0.0F));
 
-        ModelPartData tail = pyke.addChild("tail", ModelPartBuilder.create(), ModelTransform.pivot(0.0F, 0.0F, 0.0F));
+        ModelPartData tail = pyke.addChild("tail", ModelPartBuilder.create(), ModelTransform.pivot(0.0F, -0.7F, 0.0F));
 
-        ModelPartData fronttail = tail.addChild("fronttail", ModelPartBuilder.create().uv(0, 0).cuboid(-1.5F, -6.0F, 5.0F, 1.0F, 1.0F, 4.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, 0.0F, 0.0F));
+        ModelPartData fronttail = tail.addChild("fronttail", ModelPartBuilder.create().uv(0, 0).cuboid(-1.5F, -6.5F, 7.7F, 1.0F, 1.0F, 4.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, 0.0F, 0.0F));
 
-        ModelPartData backtail = tail.addChild("backtail", ModelPartBuilder.create().uv(16, 15).cuboid(-1.6F, -6.1F, 9.0F, 1.2F, 1.2F, 5.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, 0.0F, 0.0F));
+        ModelPartData backtail = tail.addChild("backtail", ModelPartBuilder.create().uv(16, 15).cuboid(-1.6F, -6.6F, 10.2F, 1.2F, 1.2F, 5.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, 0.0F, 1.2F));
         return TexturedModelData.of(modelData, 32, 32);
     }
 
@@ -71,18 +71,12 @@ public class PykeModel<T extends PykeEntity> extends SinglePartEntityModel<T> {
     @Override
     public void setAngles(T entity, float limbAngle, float limbDistance, float animationProgress, float headYaw, float headPitch) {
         this.getPart().traverse().forEach(ModelPart::resetTransform);
-        this.setHeadAngles(headYaw, headPitch);
+    
 
         this.updateAnimation(entity.idleAnimationState, ModAnimations.IDLE, 1f);
     }
 
-    private void setHeadAngles(float headYaw, float headPitch) {
-        headYaw = MathHelper.clamp(headYaw, -30.0F, 30.0F);
-        headPitch = MathHelper.clamp(headPitch, -25.0F, 45.0F);
 
-        this.head.yaw = headYaw * 0.017453292F;
-        this.head.pitch = headPitch * 0.017453292F;
-    }
 
     @Override
     public void render(MatrixStack matrices, VertexConsumer vertexConsumer, int light, int overlay, float red, float green, float blue, float alpha) {
